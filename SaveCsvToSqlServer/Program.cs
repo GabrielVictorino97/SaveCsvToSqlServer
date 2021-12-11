5d3706e1aab77ceae33874b87d23f49d122f2cd7
GabrielVictorino <gavictorino97@gmail.com>
2021-12-11 09:57:45 -0300

    Criação da api de leitura e importação dos dados para o banco de dados

---
+++ SaveCsvToSqlServer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+
+namespace SaveCsvToSqlServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var lineNumber = 0;
+                byte[] result;
+
+                using (SqlConnection connection = new SqlConnection("SQLCONNECTION"))
+                {
+                    connection.Open();
+                    string filename = @"FilePath";
+                    using (FileStream SourceStream = File.Open(filename, FileMode.Open))
+                    {
+                        result = new byte[SourceStream.Length];
+                        SourceStream.Read(result, 0, (int)SourceStream.Length);
+                    }
+                    var csv = System.Text.Encoding.ASCII.GetString(result);
+
+                    char[] delims = new[] { '\r', '\n' };
+                    string[] strings = csv.Split(delims, StringSplitOptions.RemoveEmptyEntries);
+
+                    var skipList = strings.Skip(1).ToList().Select(x => x.Split(','));
+
+                    var cmd = new SqlCommand();
+                    foreach (var values in skipList)
+                    {
+                        var sql = $"INSERT SQL";
+                        cmd.CommandText = sql;
+                        cmd.CommandType = System.Data.CommandType.Text;
+                        cmd.Connection = connection;
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    lineNumber++;
+                    connection.Close();
+                }
+
+                Console.WriteLine("Importado com sucesso!");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ocorreu um erro ao importar os dados.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
